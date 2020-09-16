7c5de7253e80a5322efff79ab2913173940ea901
Abdoul Aziz SY <azizsy88@gmail.com>
2020-09-16 17:45:51 +0200

    adding html content & function  on vote component

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VoteService,Cat,CatDetails} from '../vote.service';
+
 
 @Component({
   selector: 'app-vote',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
-  constructor() { }
+  
+  cats:Array<Cat>; // retrieve the list of cats
+  catVotedId:string; // retrieve the id of voted cat
+  voted:boolean;  // tell us if the user has voted or non
+
+  constructor(public service: VoteService) { }
 
   ngOnInit() {
+    
+    //init data for component display 
+
+    this.getAllCats();
+    this.voted=false;
+    this.catVotedId="";
+  }
+
+  //retrieve the list of all cats in database
+  getAllCats(){
+    this.service.getAllCats().subscribe((data:any)  => {
+        console.log(data)
+        this.cats=data;
+      })
+  }
+  
+
+  //Vote for a cat using vote service
+  addVoteForCat(id:string){
+    this.service.addVoteForCat(id).subscribe((data:any)  => {
+        console.log(data)
+    })
+    this.voted=true;
+    this.catVotedId=id;
+  }
+
+  //Cancel vote of a cat using vote service
+  cancelVoteForCat(id:string){
+    this.service.deleteVoteForCat(id).subscribe((data:any)  => {
+        console.log(data)
+    })
+    this.voted=false;
+    this.catVotedId="";
+  }
+
+  //Add the content of json file from latelier to database
+  addJsonDataToDatabase(){
+    this.service.getAllCatsFromLatelier().subscribe((data:any)  => {
+        console.log(data)
+        data.images.forEach(element => {
+          var cat= new Cat(element.id,element.url);
+          this.service.addCat(cat).subscribe((data:any)  => {
+            console.log(data)
+          });
+        });
+    })
+  }
+
+  //delete specific cat from the display
+  dontShowCat(id:string){
+    if(this.cats.find(item => JSON.stringify(item.id) ===JSON.stringify(id))!= undefined){
+      var catToDelete=this.cats.find(item => JSON.stringify(item.id) ===JSON.stringify(id));
+      const index = this.cats.indexOf(catToDelete, 0);
+      if (index > -1) {
+        this.cats.splice(index, 1);
+      }
+    }
   }
 
 }
