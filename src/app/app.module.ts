e5113bfd0603a771b3bd2e982268af7cef7d1c5f
Abdoul Aziz SY <azizsy88@gmail.com>
2020-09-16 15:53:54 +0200

    adding communications with backend services using HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoteComponent } from './vote/vote.component';
@@ -14,6 +15,8 @@ import { RankingComponent } from './ranking/ranking.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/vote.service.ts
+++ src/app/vote.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+//Cat class with attributs id and url
+export class Cat {
+  constructor(public id: string, public url: string) {}
+}
+
+//CatDetail class with attributs id, url, votes
+export class CatDetails {
+  constructor(public id: string, public url: string, public votes: number) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  url:string = "http://localhost:3000";
+  
+  //get all cats
+  addCat(cat: Cat) {
+    return this.httpClient.post(this.url + `/api/cats`,cat);
+  }
+
+  //get all cats
+  getAllCats() {
+    return this.httpClient.get(this.url + `/api/cats`);
+  }
+
+  //get specific cat byid
+  getCatById(id:string) {
+    return this.httpClient.get(this.url + `/api/cats/${ id }`);
+  }
+
+  //add vote for specific cat
+  addVoteForCat(id:string){
+    return this.httpClient.put(this.url + `/api/vote/cats/${ id }`,null);
+  }
+
+  //cancel vote for specific cat
+  deleteVoteForCat(id:string){
+    return this.httpClient.put(this.url + `/api/unvote/cats/${ id }`,null);
+  }
+
+
+  //get all votes order by votes
+  getAllVotes() {
+    return this.httpClient.get(this.url + `/api/votes`);
+  }
+
+  //deleteCatById
+  deleteCatById(id:string) {
+    return this.httpClient.delete(this.url + `/api/cats/${ id }`);
+  }
+
+
+  //get all cats from latelier json file
+  getAllCatsFromLatelier() {
+    return this.httpClient.get(this.url + `/api/cats/from/latelier`);
+  }
+
 }
